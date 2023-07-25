56d6828eadd336f86033743e72f61aa839f4a548
Jeffrey DeVries <jeffdevriesrocks@gmail.com>
2023-07-24 20:47:16 -0400

    [JRISC] discribed copy and stack instructions

---
+++ llvm/lib/Target/JRISCdev/JRISCdevInstrInfo.cpp
@@ -0,0 +1,50 @@
+#include "JRISCdevInstrInfo.h"
+#include "MCTargetDesc/JRISCdevTargetDesc.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/CodeGen/MachineConstantPool.h"
+#include "llvm/CodeGen/MachineFrameInfo.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/MachineMemOperand.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Function.h"
+#include "llvm/MC/MCContext.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/MC/TargetRegistry.h"
+
+#define GET_INSTRINFO_CTOR_DTOR
+#include "JRISCdevGenInstrInfo.inc"
+
+using namespace llvm;
+
+void JRISCdevInstrInfo::anchor(){}
+
+JRISCdevInstrInfo::JRISCdevInstrInfo():JRISCdevGenInstrInfo(33,34),RI(){}
+
+void JRISCdevInstrInfo::copyPhysReg(MachineBasicBlock &MBB,
+                           MachineBasicBlock::iterator MI, const DebugLoc &DL,
+                           MCRegister DestReg, MCRegister SrcReg,
+                           bool KillSrc) const{
+                            BuildMI(MBB,MI,MI->getDebugLoc(),get(JRISCdev::MOV),DestReg).addReg(SrcReg,getKillRegState(KillSrc));
+                           }
+
+void JRISCdevInstrInfo::storeRegToStackSlot(MachineBasicBlock &MBB,
+                                   MachineBasicBlock::iterator MI,
+                                   Register SrcReg, bool isKill, int FrameIndex,
+                                   const TargetRegisterClass *RC,
+                                   const TargetRegisterInfo *TRI,
+                                   Register VReg) const{
+                                    BuildMI(MBB,MI,MI->getDebugLoc(),get(JRISCdev::STWI))
+                                    .addReg(SrcReg,getKillRegState(isKill))
+                                    .addFrameIndex(FrameIndex).addImm(0);
+                                   }
+
+void JRISCdevInstrInfo::loadRegFromStackSlot(MachineBasicBlock &MBB,
+                                    MachineBasicBlock::iterator MI,
+                                    Register DestReg, int FrameIndex,
+                                    const TargetRegisterClass *RC,
+                                    const TargetRegisterInfo *TRI,
+                                    Register VReg) const{
+                                        BuildMI(MBB,MI,MI->getDebugLoc(),get(JRISCdev::LDWI),DestReg)
+                                        .addFrameIndex(FrameIndex).addImm(0);
+                                    }
---
+++ llvm/lib/Target/JRISCdev/JRISCdevInstrInfo.h
@@ -0,0 +1,41 @@
+#ifndef JRISCdev_InstrInfo_H
+#define JRISCdev_InstrInfo_H
+
+#include "JRISCdevRegisterInfo.h"
+#include "llvm/CodeGen/TargetInstrInfo.h"
+#define GET_INSTRINFO_HEADER
+#include "JRISCdevGenInstrInfo.inc"
+
+namespace llvm{
+    class JRISCdevInstrInfo : public JRISCdevGenInstrInfo{
+        const JRISCdevRegisterInfo RI;
+        virtual void anchor();
+    public:
+        JRISCdevInstrInfo();
+        const JRISCdevRegisterInfo &getRegisterInfo() const {return RI;};
+        virtual unsigned isStoreToStackSlot(const MachineInstr &MI,
+                                      int &FrameIndex) const override;
+        virtual unsigned isLoadFromStackSlot(const MachineInstr &MI,
+                                       int &FrameIndex) const override;
+
+        virtual void copyPhysReg(MachineBasicBlock &MBB,
+                           MachineBasicBlock::iterator MI, const DebugLoc &DL,
+                           MCRegister DestReg, MCRegister SrcReg,
+                           bool KillSrc) const override;
+
+        virtual void storeRegToStackSlot(MachineBasicBlock &MBB,
+                                   MachineBasicBlock::iterator MI,
+                                   Register SrcReg, bool isKill, int FrameIndex,
+                                   const TargetRegisterClass *RC,
+                                   const TargetRegisterInfo *TRI,
+                                   Register VReg) const override;
+
+        virtual void loadRegFromStackSlot(MachineBasicBlock &MBB,
+                                    MachineBasicBlock::iterator MI,
+                                    Register DestReg, int FrameIndex,
+                                    const TargetRegisterClass *RC,
+                                    const TargetRegisterInfo *TRI,
+                                    Register VReg) const override;
+    };
+}
+#endif
